bb25d1bf3fb74df31c03d2bb446e58db241356b6
Carolin19 <50742053+Carolin19@users.noreply.github.com>
2021-06-29 14:58:10 +0200

    AudioController

--- Unity/MindUpLoad/Assets/SceneController.cs
+++ Unity/MindUpLoad/Assets/SceneController.cs
@@ -8,7 +8,8 @@ public class SceneController : MonoBehaviour
     public PlayableDirector playableDirector;
     public GameObject videoPlayerLogo;
     public GameObject videoPlayerUi;
-
+    public AudioSource audioSource;
+    private float audioTime;
 
     // Start is called before the first frame update
     void Start()
@@ -30,11 +31,14 @@ public class SceneController : MonoBehaviour
             playableDirector.Play();
             videoPlayerLogo.SetActive(true);
             videoPlayerUi.SetActive(true);
+            audioSource.Play();
             Debug.Log("Start");
         }
         if (Input.GetKey(KeyCode.P))
         {
             playableDirector.Pause();
+            audioSource.Pause();
+            audioTime = audioSource.time;
             Debug.Log("Pause");
         }
         if (Input.GetKey(KeyCode.C))
@@ -42,11 +46,15 @@ public class SceneController : MonoBehaviour
             playableDirector.Stop();
             videoPlayerLogo.SetActive(false);
             videoPlayerUi.SetActive(false);
+            audioSource.Stop();
+            audioSource.time = 0;
             Debug.Log("Cancle/Restart");
         }
         if (Input.GetKey(KeyCode.R))
         {
             playableDirector.Resume();
+            audioSource.time = audioTime;
+            audioSource.Play();
             Debug.Log("Resume");
         }
     }
